8527b289c8199bde7476a7f68e8344c6429f1b97
Juan <juan.laredo@utec.edu.pe>
2023-06-13 11:10:06 -0500

    Pusheando front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+
+  const [query, setQuery] = useState('')
+  
+  const sendQuery= (e) =>{
+    e.preventDefault()
+    console.log(query)
+  } 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='app'>
+      <h2>Proyecto 2</h2>
+      <form className='query'>
+        <input type="text" id="texto" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Ingresa la query"/>
+        <button onClick={sendQuery}>Enviar</button>
+      </form>
+    </section>
   );
 }
 
