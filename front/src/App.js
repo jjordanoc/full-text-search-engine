d1a5c4a5695d3025e577867decf5850c7013d88d
Juan Diego Castro Padilla <juan.castro.p@utec.edu.pe>
2023-06-18 17:35:11 -0500

    frontend

--- front/src/App.js
+++ front/src/App.js
@@ -7,10 +7,15 @@ function App() {
   const [query, setQuery] = useState('')
   const [k, setK] = useState('')
   const [isRunning, setisRunning] = useState(false)
-  const [isfirstOpen, setisfirstOpen] = useState(true)
+  const [isRunning2, setisRunning2] = useState(false)
+  const [isfirstOpen1, setisfirstOpen1] = useState(true)
+  const [isfirstOpen2, setisfirstOpen2] = useState(true)
+
   const [elapsedTime, setelapsedTime] =  useState(0)
-  const headers = ['title', 'abstract','rank']
-  const [data, setData] = useState([])
+  const headers = ['Title', 'Abstract','Rank']
+  const headers2 = ['Title', 'Abstract','Rank']
+  const [data1, setData] = useState([])
+  const [data2, setData2] = useState([])
   
   useEffect(() => {
     let intervalId;
@@ -22,12 +27,27 @@ function App() {
     return () => clearInterval(intervalId);
   }, [isRunning])
 
+  useEffect(() => {
+    let intervalId;
+    if(isRunning2){
+      intervalId = setInterval(() =>{
+        setelapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000)
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning2])
+
   const handleStart = () =>{
     setisRunning(true)
+    setisRunning2(true)
   }
 
-  const handleStop = () =>{
-    setisRunning(false)
+  const handleStop = (n) =>{
+    if(n == 1)
+      setisRunning(false)
+    else{
+      setisRunning2(false)
+    }
   }
 
   const handleReset = () => {
@@ -36,13 +56,15 @@ function App() {
   }
 
   const sendQuery= async (e) =>{
-    setisfirstOpen(true)
+    setisfirstOpen1(true)
+    setisfirstOpen2(true)
     handleReset()
     if(e)
       e.preventDefault()
     console.log(query)
+    
     try{
-      const response = await fetch('http://localhost:8000/obtener_datos',{
+      const response = await fetch('http://localhost:8000/postgresql_recover',{
         method : "POST",
         headers: {
           'Content-Type': 'application/json',
@@ -52,18 +74,39 @@ function App() {
       if(response.ok){
         const {data: d_response} = await response.json()
         setData(d_response)
-        handleStop()
-        setisfirstOpen(false)
+        handleStop(1)
+        setisfirstOpen1(false)
       } else{
         console.log("error al obtener datos")
       }
     } catch(error){
       console.error("error en la solicitud",error)
     }
+    
+    
+    try{
+      const response = await fetch('http://localhost:8000/postgresql_recover',{
+        method : "POST",
+        headers : {
+          'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({query,k})
+      })
+      if (response.ok){
+        const {data: d_response} = await response.json()
+        setData2(d_response)
+        handleStop(2)
+        setisfirstOpen2(false)
+      } else{
+        console.log("error 2")
+      }
+    } catch(error){
+      console.error("error 2",error)
+    }
   } 
 
 
-
+  
   return (
     <section className='app'>
       <h2>Inverted Index</h2>
@@ -72,11 +115,24 @@ function App() {
         <input type='text' id='topk' value={k} onChange={(e) => setK(e.target.value)} placeholder="Top K"/>
         <button onClick={(e)=>{sendQuery(e); handleStart()}} disabled={isRunning}>Enviar</button>
       </form>
-      {!isfirstOpen && (<Tabla headers={headers} data={data}/>)}
-      <span>Elapsed Time: {elapsedTime} seconds</span>
-      {isRunning && (
-        <PacmanLoader color='#36D7B7' size={25}/>
-      )}
+      <section className='tablas'>
+      <div className='tabla-inside'>
+        {!isfirstOpen1 && <h3>Postgres</h3>}
+        {!isfirstOpen1 && (<Tabla headers={headers} data={data1}/>)}
+        <span>Elapsed Time: {elapsedTime} seconds</span>
+        {isRunning && (
+          <PacmanLoader color='#36D7B7' size={25}/>
+        )}
+      </div>
+      <div className='tabla-inside'>
+        {!isfirstOpen2 && <h3>Python</h3>}
+        {!isfirstOpen2 && (<Tabla headers={headers2} data={data2}/>)}
+        <span>Elapsed Time: {elapsedTime} seconds</span>
+        {isRunning2 && (
+          <PacmanLoader color='#36D7B7' size={25}/>
+        )}
+      </div>
+      </section>
     </section>
   );
 }
